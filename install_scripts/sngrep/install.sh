f192b2af96587525162e0ce8da165192c88235d6
tackun0301 <tackun0301@gmail.com>
2019-02-28 10:01:11 +0900

    first commit

---
+++ install_scripts/sngrep/install.sh
@@ -0,0 +1,11 @@
+cat > /etc/yum.repos.d/irontec.repo <<EOF
+[irontec]
+name=Irontec RPMs repository
+baseurl=http://packages.irontec.com/centos/$releasever/$basearch/
+EOF
+
+rpm --import http://packages.irontec.com/public.key
+
+yum install -y sngrep
+
+
